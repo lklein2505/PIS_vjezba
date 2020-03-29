22aadd32a0ab7f3c813c3d49485d617413ea47ec
lklein2505 <mario.maric.03@fesb.hr>
2020-03-29 17:16:36 +0200

    Dodana metoda

---
+++ pis_vjezba/pis_vjezba/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis_vjezba
+{
+    class NewClass
+    {
+    }
+}
